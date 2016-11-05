938f34ba6612c7c293c375daf5957963a6178ffc
Uliana Shymchak <shymchak1@gmail.com>
2016-11-05 11:23:25 +0200

    Added demo fileforbehaviour patterns

---
+++ GofPatternsFun/Behavioral/BehaviourPatternsDemo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GofPatternsFun.Behavioral
+{
+    class BehaviourPatternsDemo
+    {
+    }
+}
